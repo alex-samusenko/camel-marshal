85a73edf7302f51f1c9653e8ca2dd3c13d982399
Aleksey Samusenko <alex.samusenko@gmail.com>
2021-07-21 23:06:25 +0300

    Second commit

--- src/main/java/com/javainuse/model/ItemValue.java
+++ src/main/java/com/javainuse/model/ItemValue.java
@@ -3,12 +3,13 @@ package com.javainuse.model;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
 
 @XmlRootElement(name = "itemvalue")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ItemValue {
     public String answerstr;
     public double answernumber;
-    public String answerdate;
+    public Date answerdate;
     public String answercode;
 }
--- src/main/java/com/javainuse/processor/MyProcessor.java
+++ src/main/java/com/javainuse/processor/MyProcessor.java
@@ -4,10 +4,28 @@ import com.javainuse.model.*;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import java.util.Date;
+
 public class MyProcessor implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
 		Documents documents = exchange.getIn().getBody(Documents.class);
+		String ttnNum;
+		Date ttnDate;
+		for (Survey survey : documents.surveys.survey) {
+			int docQty = 0;
+			for (Item item : survey.body.item) {
+				if (item.SKUcode == "ttnnum") {
+					ttnNum = item.itemvalues.itemvalue.answerstr;
+				}
+				else if (item.SKUcode == "ttndate") {
+					ttnDate = item.itemvalues.itemvalue.answerdate;
+				}
+				else if (item.topiccode == "returninvoicenumber") {
+					docQty++;
+				}
+			}
+		}
 	}
 
 }
